5af2d5dd11c3e795b24110ed259584c2cfce9142
weyjei <52375862+weyjei@users.noreply.github.com>
2023-09-28 11:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ from tqdm import tqdm
 import concurrent.futures
 from moviepy.editor import VideoFileClip
 
-from nsfw_checker import NSFWChecker
 from face_swapper import Inswapper, paste_to_whole
 from face_analyser import detect_conditions, get_analysed_data, swap_options_list
 from face_parsing import init_parsing_model, get_parsed_mask, mask_regions, mask_regions_to_list
@@ -70,7 +69,6 @@ FACE_SWAPPER = None
 FACE_ANALYSER = None
 FACE_ENHANCER = None
 FACE_PARSER = None
-NSFW_DETECTOR = None
 FACE_ENHANCER_LIST = ["NONE"]
 FACE_ENHANCER_LIST.extend(get_available_enhancer_names())
 FACE_ENHANCER_LIST.extend(cv2_interpolations)
@@ -117,11 +115,6 @@ def load_face_parser_model(path="./assets/pretrained_models/79999_iter.pth"):
     global FACE_PARSER
     if FACE_PARSER is None:
         FACE_PARSER = init_parsing_model(path, device=device)
-
-def load_nsfw_detector_model(path="./assets/pretrained_models/open-nsfw.onnx"):
-    global NSFW_DETECTOR
-    if NSFW_DETECTOR is None:
-        NSFW_DETECTOR = NSFWChecker(model_path=path, providers=PROVIDER)
 
 
 load_face_analyser_model()
@@ -194,9 +187,6 @@ def process(
 
     ## ------------------------------ PREPARE INPUTS & LOAD MODELS ------------------------------
 
-    yield "### \n ⌛ Loading NSFW detector model...", *ui_before()
-    load_nsfw_detector_model()
-
     yield "### \n ⌛ Loading face analyser model...", *ui_before()
     load_face_analyser_model()
 
@@ -227,15 +217,6 @@ def process(
 
     def swap_process(image_sequence):
         ## ------------------------------ CONTENT CHECK ------------------------------
-
-        yield "### \n ⌛ Checking contents...", *ui_before()
-        nsfw = NSFW_DETECTOR.is_nsfw(image_sequence)
-        if nsfw:
-            message = "NSFW Content detected !!!"
-            yield f"### \n 🔞 {message}", *ui_before()
-            assert not nsfw, message
-            return False
-        EMPTY_CACHE()
 
         ## ------------------------------ ANALYSE FACE ------------------------------
 
